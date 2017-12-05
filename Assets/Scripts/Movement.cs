a9da085ff33fc12f2a3028e400f89e0230318149
TheNuttyGamer <33847045+TheNuttyGamer@users.noreply.github.com>
2017-12-05 20:38:54 +0000

    Customer Spawning added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,24 +9,28 @@ public class Movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        //Forward Movement
 	    if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(new Vector3(0, 1 * speed, 0));
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
 
+        //Left Movement
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(new Vector3(0, 1 * speed, 0));
             transform.rotation = Quaternion.Euler(0, 0, 90);
         }
 
+        //Right Movement
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(new Vector3(0, 1 * speed, 0));
             transform.rotation = Quaternion.Euler(0, 0, -90);
         }
 
+        //Down Movement
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(new Vector3(0, 1 * speed, 0));
--- Assets/Scripts/PickTable.cs
+++ Assets/Scripts/PickTable.cs
@@ -3,12 +3,30 @@ using System.Collections;
 
 public class PickTable : MonoBehaviour {
 
+    //Declare Variables
+    [SerializeField]
+    private int spawnInterval;
+
     private GameObject[] tables;
     private GameObject pickedTable;
+    [SerializeField]
+    private GameObject customer;
     private SpriteRenderer spriteRenderer;
 
-	// Use this for initialization
-	void Start ()
+
+    private IEnumerator SpawnCustomer(int interval, GameObject customer)
+    {
+        if (pickedTable.transform.childCount < 1)
+        {
+            Instantiate(customer, new Vector3(pickedTable.transform.position.x, pickedTable.transform.position.y - 1), Quaternion.identity, pickedTable.transform);
+        }
+
+        yield return new WaitForSeconds(interval);
+
+    }
+
+    // Use this for initialization
+    void Start ()
     {
         tables = GameObject.FindGameObjectsWithTag("Table");
     }
@@ -18,7 +36,7 @@ public class PickTable : MonoBehaviour {
     {
         pickedTable = chooseTable(tables);
 
-        print(pickedTable);
+        StartCoroutine(SpawnCustomer(spawnInterval, customer));
 	}
 
     private GameObject chooseTable(GameObject[]tablesArray)
