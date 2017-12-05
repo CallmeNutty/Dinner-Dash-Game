bdcf4ac12859d0269aaa73ae543698a3b2235be0
TheNuttyGamer <33847045+TheNuttyGamer@users.noreply.github.com>
2017-12-05 21:14:25 +0000

    Started text bubbles

---
+++ Assets/Scripts/CustomerDetection.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomerDetection : MonoBehaviour
+{
+
+    [SerializeField]
+    private Canvas speechBubble;
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        speechBubble.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D coll)
+    {
+        speechBubble.gameObject.SetActive(false);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
