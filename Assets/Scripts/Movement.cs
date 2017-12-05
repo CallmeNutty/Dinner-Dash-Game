53061652f6cef95e9bf7529ad1f34fcaa085a5d4
TheNuttyGamer <33847045+TheNuttyGamer@users.noreply.github.com>
2017-12-05 19:03:18 +0000

    Player Rotation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,12 +5,6 @@ public class Movement : MonoBehaviour {
 
     [SerializeField]
     private float speed;
-
-	// Use this for initialization
-	void Start ()
-    {
-	
-	}
 	
 	// Update is called once per frame
 	void Update ()
@@ -18,21 +12,25 @@ public class Movement : MonoBehaviour {
 	    if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(new Vector3(0, 1 * speed, 0));
+            transform.rotation = Quaternion.Euler(0, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(new Vector3(-1 * speed, 0, 0)); ;
+            transform.Translate(new Vector3(0, 1 * speed, 0));
+            transform.rotation = Quaternion.Euler(0, 0, 90);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(new Vector3(1 * speed, 0, 0)); ;
+            transform.Translate(new Vector3(0, 1 * speed, 0));
+            transform.rotation = Quaternion.Euler(0, 0, -90);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(new Vector3(0, -1 * speed, 0)); ;
+            transform.Translate(new Vector3(0, 1 * speed, 0));
+            transform.rotation = Quaternion.Euler(0, 0, 180);
         }
     }
 }
