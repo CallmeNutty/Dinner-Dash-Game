cf4f3f95bd4829003307beac97dc21dddef5e61f
TheNuttyGamer <33847045+TheNuttyGamer@users.noreply.github.com>
2017-12-05 20:04:47 +0000

    Pick Table system implemented

---
+++ Assets/Scripts/PickTable.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickTable : MonoBehaviour {
+
+    private GameObject[] tables;
+    private GameObject pickedTable;
+    private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        tables = GameObject.FindGameObjectsWithTag("Table");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        pickedTable = chooseTable(tables);
+
+        print(pickedTable);
+	}
+
+    private GameObject chooseTable(GameObject[]tablesArray)
+    {
+        return tablesArray[Random.Range(0, tablesArray.Length)];
+    }
+}
